b6d861c3116810bf7fa602aa45db3a58433e0e1b
Vic <z5310452@ad.unsw.edu.au>
2026-06-12 09:48:07 +1000

    Added creating manual folders util

---
+++ core/utils/create_manual_folders.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+workspace_path = Path("F:\\ALA Projects\\Pixie Dust\\sheeping_beauty\\03_production")
+
+def create_manual_folders(main_folder: Path):
+    # Create manual folders where there were previous none with wip and publishes folders.
+    # Very rare use
+    if any(item.is_dir() for item in main_folder.iterdir()):
+        for folder in main_folder.iterdir():
+            if (any(child.is_dir() for child in folder.glob("wip"))
+                and any(child.is_dir() for child in folder.glob("publishes"))):
+                manual_path = folder / "manual"
+                manual_path.mkdir()
+            create_manual_folders(folder)
+
+create_manual_folders(workspace_path)
