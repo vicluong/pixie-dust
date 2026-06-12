0f1fae5d784375f5d0f78a1eb6b123c9b25e1b17
Vic <z5310452@ad.unsw.edu.au>
2026-06-12 14:20:48 +1000

    Created another config for potential different uses of pixie dust

--- core/env_vars.py
+++ core/env_vars.py
@@ -7,4 +7,4 @@ env_config = os.environ.get("PIXIE_CONFIG_PATH")
 if env_config:
 	CONFIG_PATH = Path(env_config)
 else:
-	CONFIG_PATH = BASE_DIR.parent / "config.json"
+	CONFIG_PATH = BASE_DIR.parent / "config_vvc.json"
