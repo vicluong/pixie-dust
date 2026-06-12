9c24fd5a05b6612c1ab9893dbdbdb75f8490aa22
Vic <z5310452@ad.unsw.edu.au>
2026-06-12 15:17:21 +1000

    Fixed bug in create_manual_folders

--- core/utils/create_manual_folders.py
+++ core/utils/create_manual_folders.py
@@ -6,8 +6,10 @@ workspace_path = Path("F:\\ALA Projects\\Pixie Dust\\sheeping_beauty\\03_product
 def create_manual_folders(main_folder: Path):
     # Create manual folders where there were previous none with wip and publishes folders.
     # Very rare use
-    if any(item.is_dir() for item in main_folder.iterdir()):
-        for folder in main_folder.iterdir():
+    subdirs = [f for f in main_folder.iterdir() if f.is_dir()]
+
+    for folder in subdirs:
+        if folder.is_dir():
             if (any(child.is_dir() for child in folder.glob("wip"))
                 and any(child.is_dir() for child in folder.glob("publishes"))):
                 manual_path = folder / "manual"
