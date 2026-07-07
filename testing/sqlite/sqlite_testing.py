842d99cf1077182574b4ca68f83cde6b55923b1e
Victor Luong <victor.luong@student.uts.edu.au>
2026-07-07 16:39:54 +1000

    Added a few tables for sql

--- testing/sqlite/sqlite_testing.py
+++ testing/sqlite/sqlite_testing.py
@@ -1,36 +1,105 @@
 import sqlite3
 
-sql_statements = [ 
-    """CREATE TABLE IF NOT EXISTS projects (
-            id INTEGER PRIMARY KEY, 
-            name text NOT NULL, 
-            begin_date DATE, 
-            end_date DATE
-        );""",
+create_sql_statements = [ 
+    """
+    CREATE TABLE IF NOT EXISTS Projects (
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL,
+        status TEXT NOT NULL,
+        start_date TEXT NOT NULL, 
+        end_date TEXT NOT NULL,
+        description TEXT,
+        image TEXT
+    );
+    """,
 
-    """CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY, 
-            name TEXT NOT NULL, 
-            priority INT, 
-            project_id INT NOT NULL, 
-            status_id INT NOT NULL, 
-            begin_date DATE NOT NULL, 
-            end_date DATE NOT NULL, 
-            FOREIGN KEY (project_id) REFERENCES projects (id)
-        );"""
+    """
+    CREATE TABLE IF NOT EXISTS Sequences (
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL, 
+        project_id INT NOT NULL, 
+        description TEXT,
+        FOREIGN KEY (project_id) REFERENCES projects (id)
+            ON DELETE CASCADE 
+            ON UPDATE CASCADE
+    );
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS Shots (
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL, 
+        sequence_id INT NOT NULL, 
+        description TEXT,
+        image TEXT,
+        FOREIGN KEY (sequence_id) REFERENCES sequences (id)
+            ON DELETE CASCADE 
+            ON UPDATE CASCADE
+    );
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS Assets (
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL, 
+        project_id INT NOT NULL, 
+        description TEXT,
+        asset_type TEXT NOT NULL,
+        image TEXT,
+        FOREIGN KEY (project_id) REFERENCES projects (id)
+            ON DELETE CASCADE 
+            ON UPDATE CASCADE
+    );
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS Entities (
+        id INTEGER PRIMARY KEY, 
+        entity_type TEXT NOT NULL, 
+        entity_id INT NOT NULL
+    );
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS Tasks (
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL, 
+        priority INT, 
+        project_id INT NOT NULL, 
+        entity_id INT NOT NULL,
+        status_id INT NOT NULL, 
+        start_date TEXT NOT NULL, 
+        end_date TEXT NOT NULL, 
+        latest_publish TEXT,
+        FOREIGN KEY (project_id) REFERENCES projects (id)
+            ON DELETE CASCADE 
+            ON UPDATE CASCADE
+        FOREIGN KEY (entity_id) REFERENCES entities (id)
+            ON DELETE CASCADE 
+            ON UPDATE CASCADE
+    );
+    """
+]
+
+insert_sql_statements = [ 
+    """
+    INSERT INTO Projects
+    VALUES
+    ('1', 's126', 'in-progress', datetime('now'), datetime('now','+5 day','localtime'), 'Project of 2026', 'image.png');
+    """
 ]
 
 # create a database connection
 try:
     with sqlite3.connect('my.db') as conn:
-        # create a cursor
         cursor = conn.cursor()
 
-        # execute statements
-        for statement in sql_statements:
+        for statement in create_sql_statements:
             cursor.execute(statement)
 
-        # commit the changes
+        for statement in insert_sql_statements:
+            cursor.execute(statement)
+
         conn.commit()
 
         print("Tables created successfully.")
